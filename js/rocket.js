46f50b1c6e869ffe1431391ec53ac794db742943
nurjannah20 <nur-jannah-bs@hotmail.com>
2020-05-31 21:07:07 +0800

    Made our Cake Rocket Launcher!

--- js/rocket.js
+++ js/rocket.js
@@ -13,17 +13,32 @@ var changeState = function (state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber-1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber > 4 && countdownNumber <=7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else {
+				document.getElementById('nervous').className = 'nervous';
+			}
+			
+			if (countdownNumber <= 4 && countdownNumber >1) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+			
 			if (countdownNumber <= 0) { //Instead of <= 0, changed it to <= -2 so that Lift Off! can appear after countdown reaches 0.
 				changeState(3); // Lift Off!
 			}
-		}, 1000)	
+		}, 500)	
 	} else if (state == 3) {
 		var success = setTimeout(function () {
 			var randomNumber = Math.round(Math.random()*10);
 			
 			console.log('randomNumber: '+randomNumber);
 			
-			if (randomNumber > 5) {
+			if (randomNumber > 4) {
 				changeState(4); // Well Done!
 			} else {
 				changeState(5); // Oh No!!!
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 // JavaScript Document
+setTimeout(function () {
+	location.href = 'rocket.html';
+}, 2000);
+
 var doCoolStuff = function () {
 	var currentClassName = document.getElementById('cool').className;
 	// Changing class name
